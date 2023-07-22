b460bd4b989ad9aa99385786efe36ad5abab4b3c
Om Londhe <oplondhe@gmail.com>
2023-07-22 11:56:40 +0530

    Completed with V1.0.0

---
+++ dist/esm/types/components/MultiRangeSlider/MultiRangeSlider.stories.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+declare const _default: {
+    title: string;
+    parameters: {
+        description: string;
+    };
+};
+export default _default;
+export declare const MultiRangeSliderDefault: () => React.JSX.Element;
---
+++ dist/esm/types/components/MultiRangeSlider/types.d.ts
@@ -0,0 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+export interface MultiRangeSliderProps {
+    min: number;
+    max: number;
+    value: number[];
+    onChange: Dispatch<SetStateAction<number[]>>;
+    height?: number;
+    sliderInactiveBackgroundColor?: string;
+    sliderActiveBackgroundColor?: string;
+    sliderHeight?: number;
+    sliderInactiveRadius?: number;
+    sliderActiveRadius?: number;
+    thumbColor?: string;
+    thumbRadius?: number;
+    thumbBorder?: string;
+    thumbHeight?: number;
+    thumbWidth?: number;
+}
